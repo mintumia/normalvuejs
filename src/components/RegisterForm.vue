436366b93fca08b4f750f03ab257151613c0b5d8
PROPERTY\214843 <mintumia8@gmail.com>
2023-08-24 19:37:44 +0600

    Checkbox Update from Tailwind Plugins

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -2,28 +2,54 @@
 import {defineComponent} from 'vue'
 
 export default defineComponent({
-  name: "RegisterForm"
+  name: "RegisterForm",
+  data() {
+
+    return{
+      login:{
+        name:'',
+        email:'',
+        passport:'',
+        technology:'',
+        python:''
+      }
+    }
+
+
+  }
+
+
 })
 </script>
 
 <template>
   <div class="min-h-screen bg-gray-100 flex flex-col justify-center">
     <div class="lg:w-2/4 md:w-3/4 sm:w-full mx-auto">
-      <label for="name">Enter Your Name:</label>
-      <input type="text" name="name" id="name">
-      <label for="Email">Enter Email:</label>
-      <input type="Email" name="name" id="email">
-      <button
-          class="w-full flex justify-center my-4 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          type="submit">Submit
-      </button>
+      <form >
+        <label for="name" >Enter Your Name:</label>
+        <input type="text" name="name" id="name" v-model.lazy="login.name">
+        <label for="email">Enter Email:</label>
+        <input type="email" name="email" id="email" v-model.lazy="login.email">
+        <label for="pass">Password :</label>
+        <input type="password" name="name" id="pass" v-model.lazy="login.passport">
+        <label for="technology">Java : </label>
+        <input class="ring-1 ring-green-500 mr-3" type="checkbox" name="java" value="java" id="technology" v-model.lazy="login.technology">
+        <label for="python">Python : </label>
+        <input class="mr-3" type="checkbox" name="python" value="python" id="python" v-model.lazy="login.python">
+        <button
+            class="w-full flex justify-center my-4 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            type="submit">Submit
+        </button>
 
+      </form>
 
 
     </div>
 
+    <div class="text-2xl font-bold">{{login}}</div>
+    </div>
 
-  </div>
+
 
 </template>
 
