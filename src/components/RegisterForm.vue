863dc3eada58ea8488f5a91b3d8b6be93239e95b
PROPERTY\214843 <mintumia8@gmail.com>
2023-08-27 13:58:00 +0600

    Tailwind App.css Updated

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -10,11 +10,20 @@ export default defineComponent({
         name:'',
         email:'',
         passport:'',
-        technology:'',
-        python:''
+        technology:[],
+
       }
     }
 
+
+  },
+  methods:{
+    formSub(){
+
+      console.warn('Form Submited',this.login);
+     // this.$refs.regiform.preventDefault();
+
+    }
 
   }
 
@@ -25,7 +34,7 @@ export default defineComponent({
 <template>
   <div class="min-h-screen bg-gray-100 flex flex-col justify-center">
     <div class="lg:w-2/4 md:w-3/4 sm:w-full mx-auto">
-      <form >
+      <form ref="regiform" >
         <label for="name" >Enter Your Name:</label>
         <input type="text" name="name" id="name" v-model.lazy="login.name">
         <label for="email">Enter Email:</label>
@@ -33,15 +42,14 @@ export default defineComponent({
         <label for="pass">Password :</label>
         <input type="password" name="name" id="pass" v-model.lazy="login.passport">
         <label for="technology">Java : </label>
-        <input class="ring-1 ring-green-500 mr-3" type="checkbox" name="java" value="java" id="technology" v-model.lazy="login.technology">
+        <input class="mr-3" type="checkbox" name="java" value="java" id="technology" v-model.lazy="login.technology">
         <label for="python">Python : </label>
-        <input class="mr-3" type="checkbox" name="python" value="python" id="python" v-model.lazy="login.python">
-        <button
-            class="w-full flex justify-center my-4 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            type="submit">Submit
-        </button>
-
+        <input class="mr-3" type="checkbox" name="python" value="python" id="python" v-model.lazy="login.technology">
+        <button class="my-4" v-on:click.prevent="formSub" type="button">Submit </button>
+        <button class="my-4 bg-green-500 hover:bg-red-300" type="reset">Reset All</button>
       </form>
+
+
 
 
     </div>
