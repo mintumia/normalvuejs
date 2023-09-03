318f58901bc00002da23e3875707eacb5f448351
PROPERTY\214843 <mintumia8@gmail.com>
2023-09-03 16:56:05 +0600

    Vue Router Plugins Added

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
 <template>
+<div class="flex flex-col justify-center">
+  <button class="bg-blue-500 text-white my-2 py-3" @click="tab='HelloWorld'">com01</button>
+  <button class="bg-blue-500 text-white my-2 py-3" @click="tab='RegisterForm'">com01</button>
+  <button class="bg-blue-500 text-white my-2 py-3" v-on:click="tab='RegisterForm'" >Click Me</button>
+</div>
 
-<!--  <HelloWorld msg="Welcome to Mintu Page"/>-->
-<!--  <MintuParent/>-->
-<!--  <RuffTest/>-->
-  <RegisterForm/>
+  <component :is="tab"/>
+
 </template>
 
 <script>
-/*import HelloWorld from './components/HelloWorld.vue'*/
-/*import MintuParent from './components/MintuParent.vue';*/
+import HelloWorld from './components/HelloWorld.vue'
+/*
+import MintuParent from './components/MintuParent.vue';
+*/
 
 /*import RuffTest from "@/components/RuffTest.vue";*/
 import RegisterForm from "@/components/RegisterForm.vue";
+/*import MintuSlots from "@/components/MintuSlots.vue";*/
 
 
 export default {
   name: 'App',
- components: {
-    /*HelloWorld,*/
-    // MintuParent,
-   RegisterForm,
-   /*RuffTest*/
+  data(){
+    return{
+      tab:'HelloWorld',
+      count:0,
+    }
+
+  },
+  beforeUpdate() {
+    console.log("before : " + this.count);
+  },
+  updated() {
+    console.log("Updated : " + this.count++);
+  },
+  beforeUnmount() {
+    console.log("before Unmount : " + this.count--);
+  },
+
+  components: {
+    /*MintuSlots,*/
+    HelloWorld,
+   /* MintuParent,*/
+    RegisterForm,
+    /*RuffTest*/
   }
 
 }
---
+++ src/components/MintuSlots.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="inline-flex w-2/4 text-center mx-auto p-4 border-2 border-indigo-400 rounded rounded-2xl my-2 shadow-lg shadow-black">
+    <header class="text-2xl font-bold">
+      <slot name="header">
+
+      </slot>
+    </header>
+    <content class="text-sm">
+      <slot name="main">
+
+      </slot>
+    </content>
+    <footer class="bg-blue-500">
+      <slot name="footer">
+
+      </slot>
+    </footer>
+
+
+  </div>
+
+
+</template>
+
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "MintuSlots",
+  props: {
+    name: String,
+
+  }
+
+})
+
+</script>
